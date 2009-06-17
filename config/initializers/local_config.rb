084473adb8a1594f4fd81d4371d1d25ac8499625
Jorge Calás Lozano <calas@qvitta.net>
2009-06-17 21:39:10 +0200

    New rails way in routes and Rails.root use

--- config/initializers/local_config.rb
+++ config/initializers/local_config.rb
@@ -1,14 +1,14 @@
 # To ease development local configuration is not in SVN.
 # A file config/local_config.rb is expected with configuration,
 # see config/local_config.rb.example.
-local_config_rb = File.join(RAILS_ROOT, 'config', 'local_config.rb')
+local_config_rb = Rails.root.join('config', 'local_config.rb')
 if not File.exists?(local_config_rb)
   puts "Missing config/local_config.rb, aborting"
   exit 1
 end
 require local_config_rb
 
-facturagem_yml = File.join(RAILS_ROOT, 'config', 'facturagem.yml')
+facturagem_yml = Rails.root.join('config', 'facturagem.yml')
 if not File.exists?(facturagem_yml)
   puts "Missing config/facturagem.yml, aborting"
   exit 1
@@ -16,7 +16,7 @@ end
 
 CONFIG = YAML::load(File.open(facturagem_yml))
 
-mailer_rb = File.join(RAILS_ROOT, 'config', 'mailer.rb')
+mailer_rb = Rails.root.join('config', 'mailer.rb')
 if not File.exists?(mailer_rb)
   puts "Missing config/mailer.rb, aborting"
   exit 1
