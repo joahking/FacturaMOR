eb18709155111ddcc46d525652d009ff997d608e
Jorge Calás Lozano <calas@qvitta.net>
2009-06-17 21:36:04 +0200

    cleaning up account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,31 +1,31 @@
 # == Schema Information
-# Schema version: 7
 #
 # Table name: accounts
 #
-#  id               :integer(11)   not null, primary key
-#  owner_id         :integer(11)   
-#  short_name       :string(255)   not null
-#  blocked          :boolean(1)    not null
-#  name             :string(255)   not null
-#  name_for_sorting :string(255)   
-#  bank_account     :string(255)   
-#  direct_login     :boolean(1)    
-#  referer          :string(1024)  
-#  landing_page     :string(1024)  
-#  created_at       :datetime      
-#  updated_at       :datetime      
+#  id               :integer(4)      not null, primary key
+#  owner_id         :integer(4)
+#  short_name       :string(255)     not null
+#  blocked          :boolean(1)      not null
+#  name             :string(255)     not null
+#  name_for_sorting :string(255)
+#  bank_account     :string(255)
+#  direct_login     :boolean(1)
+#  referer          :string(1024)
+#  landing_page     :string(1024)
+#  created_at       :datetime
+#  updated_at       :datetime
 #
 
 class Account < ActiveRecord::Base
-  attr_protected :direct_login, :owner_id, :is_blocked, :is_active, :bank_account, :referer, :landing_page
-  
-  belongs_to :owner, :class_name => 'User', :foreign_key => :owner_id
-  
-  # The address is the one in fiscal_data, there was a time where we wanted
-  # to have the separate, but after that we simplified this assumption and
-  # the fiscal data for *our* invoices to the user is assumed to be the same
-  # fiscal date he uses in the application
+  attr_protected :direct_login, :owner_id, :is_blocked, :is_active
+  attr_protected :bank_account, :referer, :landing_page
+
+  belongs_to :owner, :class_name => 'User'
+
+  # The address is the one in fiscal_data, there was a time where we wanted to
+  # have the separate, but after that we simplified this assumption and the
+  # fiscal data for *our* invoices to the user is assumed to be the same fiscal
+  # date he uses in the application
   #
   # has_one :address, :as => :addressable, :dependent => :destroy
   has_one :fiscal_data, :dependent => :destroy
@@ -35,18 +35,19 @@ class Account < ActiveRecord::Base
   has_many :invoices, :dependent => :destroy
 
   has_one :login_token_for_agencies, :dependent => :destroy
+
   after_create :renew_login_token_for_agencies
-  
+
   has_one :chpass_token, :dependent => :destroy
 
   add_for_sorting_to :name
-  
+
   validates_presence_of   :name
   validates_presence_of   :short_name
   validates_uniqueness_of :short_name
   validates_exclusion_of  :short_name, :in => CONFIG['reserved_subdomains'], :message => 'esta es una dirección reservada'
   validates_format_of     :short_name, :with => %r{\A[a-z][\-a-z\d]+\z}, :message => 'la dirección debe empezar por una letra y sólo puede tener caracteres alfanumericos'
-  
+
   # We do not validate the existence of the owner because of the
   # chicken and egg problem in the public signup, that action
   # must ensure there's a owner.
@@ -75,15 +76,15 @@ class Account < ActiveRecord::Base
   def last_invoice
     last_invoices(1).first
   end
-  
+
   def last_invoices(n)
     invoices.sort.slice(0, n)
   end
-  
+
   def not_yet_paid_amount
     Invoice.sum('total', :conditions => {:paid => false, :account_id => id})
   end
-  
+
   def renew_login_token_for_agencies
     # We loop because of the UNIQUE constraint on the token column.
     loop do
@@ -107,7 +108,7 @@ class Account < ActiveRecord::Base
       end
     end
   end
-  
+
   def has_logo?
     fiscal_data && fiscal_data.logo
   end
